c4b67509c5dc10ac07a6a988f712fba2d8b41153
Damien Wuterich <damienwuterich@gmail.com>
2023-02-28 18:26:01 -0800

    Create tracks with sound & timeline

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
 import "./App.sass";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="track">
+        <div className="sound"></div>
+        <div className="timeline">
+          <div>1</div>
+          <div>&</div>
+          <div>2</div>
+          <div>&</div>
+          <div>3</div>
+          <div>&</div>
+          <div>4</div>
+          <div>&</div>
+        </div>
+      </div>
+      <div className="track">
+        <div className="sound"></div>
+        <div className="timeline">
+          <div>1</div>
+          <div>&</div>
+          <div>2</div>
+          <div>&</div>
+          <div>3</div>
+          <div>&</div>
+          <div>4</div>
+          <div>&</div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;

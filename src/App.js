235c260ea318eb55dc34c37227fa89b845afae5f
Damien Wuterich <damienwuterich@gmail.com>
2023-03-01 14:19:43 -0800

    Create Track component

--- src/App.js
+++ src/App.js
@@ -1,34 +1,28 @@
 import "./App.sass";
 
+function Track() {
+  return (
+    <div className="track">
+      <div className="sound"></div>
+      <div className="timeline">
+        <div>1</div>
+        <div>&</div>
+        <div>2</div>
+        <div>&</div>
+        <div>3</div>
+        <div>&</div>
+        <div>4</div>
+        <div>&</div>
+      </div>
+    </div>
+  );
+}
+
 function App() {
   return (
     <div className="App">
-      <div className="track">
-        <div className="sound"></div>
-        <div className="timeline">
-          <div>1</div>
-          <div>&</div>
-          <div>2</div>
-          <div>&</div>
-          <div>3</div>
-          <div>&</div>
-          <div>4</div>
-          <div>&</div>
-        </div>
-      </div>
-      <div className="track">
-        <div className="sound"></div>
-        <div className="timeline">
-          <div>1</div>
-          <div>&</div>
-          <div>2</div>
-          <div>&</div>
-          <div>3</div>
-          <div>&</div>
-          <div>4</div>
-          <div>&</div>
-        </div>
-      </div>
+      <Track />
+      <Track />
     </div>
   );
 }

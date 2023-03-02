3f89c732b7759fead0638d72a0e9ec2978c5226a
Damien Wuterich <damienwuterich@gmail.com>
2023-03-01 16:02:51 -0800

    WIP Implement playback loop

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import { useState } from "react";
 import "./App.sass";
 
 const times = [...Array(8).keys()].map((i) => 1 + i / 2);
+const increment = times[1] - times[0];
 
-function Track() {
+function Track({ track }) {
   return (
     <div className="track">
       <select className="sound">
@@ -12,18 +13,55 @@ function Track() {
       </select>
       <div className="timeline">
         {times.map((time) => (
-          <div key={time}>{time}</div>
+          <div key={time} className={track[time] ? "selected" : ""}>
+            {time}
+          </div>
         ))}
       </div>
     </div>
   );
 }
 
-function App() {
+function playSound(sound) {
+  new Audio(`beat-layers/sounds/${sound}`).play();
+}
+
+export default function App() {
   const [isPlaying, setIsPlaying] = useState(false);
+  const [currentTime, setCurrentTime] = useState(times[0]);
+  const [tracks, setTracks] = useState([
+    { sound: "KbdSpaceBarModernUX.wav", 1: true, 3: true },
+    { sound: "KbdFunction.wav", 2: true, 4.5: true },
+  ]);
+  const [timeoutID, setTimeoutID] = useState(null);
+
+  function play() {
+    console.log(currentTime);
+    for (const track of tracks) {
+      if (track[currentTime]) {
+        playSound(track.sound);
+      }
+    }
+
+    const newTimeoutID = setTimeout(incrementTimeThenPlay, 1000 * increment);
+    setTimeoutID(newTimeoutID);
+  }
+
+  function incrementTimeThenPlay() {
+    if (currentTime === 4.5) setCurrentTime(1);
+    else setCurrentTime(currentTime + increment);
+    play();
+  }
 
   function handlePlayPauseToggle() {
-    setIsPlaying(!isPlaying);
+    if (isPlaying) {
+      clearTimeout(timeoutID);
+      setTimeoutID(null);
+      setIsPlaying(false);
+    } else {
+      play();
+      setIsPlaying(true);
+    }
   }
 
   return (
@@ -31,10 +69,9 @@ function App() {
       <button onClick={handlePlayPauseToggle}>
         {isPlaying ? "pause" : "play"}
       </button>
-      <Track />
-      <Track />
+      {tracks.map((track, i) => (
+        <Track key={i} track={track} />
+      ))}
     </div>
   );
 }
-
-export default App;
